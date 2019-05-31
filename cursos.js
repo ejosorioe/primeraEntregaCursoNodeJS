df7448517293f1638962b58592793fe7b46357cc
Erika <ejosorioe@unal.edu.co>
2019-05-30 21:24:29 -0500

    Bonus - Mostrar resultados tarea 1 a través de un navegador

--- cursos.js
+++ cursos.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const express = require('express')
+const app = express()
 
 const cursos = [{
                     id: 1,
@@ -21,38 +23,57 @@ const cursos = [{
                     valor: '350000',
                     demand: true,
                 }]
-
+/*
 setTimeout(function(){
-console.log('el curso con id ' +cursos[0].id+' se llama '+cursos[0].nombre+' tiene una duracion de '+
+console.log('el curso con id ' +cursos[0].id+' llamado '+cursos[0].nombre+' tiene una duracion de '+
 cursos[0].duracion+' y cuesta '+cursos[0].valor+' pesos');
 },2000);
 
 setTimeout(function(){
-    console.log('el curso con id ' +cursos[1].id+' se llama '+cursos[1].nombre+' tiene una duracion de '+
+    console.log('el curso con id ' +cursos[1].id+' llamado '+cursos[1].nombre+' tiene una duracion de '+
     cursos[1].duracion+' y cuesta '+cursos[1].valor+' pesos');
     },4000);
 
 setTimeout(function(){
-    console.log('el curso con id ' +cursos[2].id+' se llama '+cursos[2].nombre+' tiene una duracion de '+
+    console.log('el curso con id ' +cursos[2].id+' llamado '+cursos[2].nombre+' tiene una duracion de '+
     cursos[2].duracion+' y cuesta '+cursos[2].valor+' pesos');
     },6000);    
+*/
+
+function doSetTimeout(i) 
+{
+    setTimeout(function(){
+        console.log('el curso con id ' +cursos[i]['id']+' llamado '+cursos[i]['nombre']+' tiene una duracion de '+
+        cursos[i]['duracion']+' y cuesta '+cursos[i]['valor']+' pesos');
+    },2000*(i+1));
+}
+
+for(var i = 0; i < cursos.length;i++)
+    doSetTimeout(i)
 
 let matricularAlumno = (id_busqueda,nombreEstudiante,cedula) => {
     if (id_busqueda != undefined){
         let curso = cursos.find( cursos => cursos.id == id_busqueda);
-        //console.log(curso);
+
         if (curso){
-            let descripcionCurso = 'El curso con id ' +curso.id+' se llama '+curso.nombre+' tiene una duracion de '+
+            let descripcionCurso = 'el curso con id ' +curso.id+' llamado '+curso.nombre+' tiene una duracion de '+
             curso.duracion+' y cuesta '+curso.valor+' pesos'
             console.log(descripcionCurso);
-            let texto = 'El estudiante '+nombreEstudiante+'\n'+
-            'con cedula '+ cedula + '\n'+
-            'se ha matriculado en ' + descripcionCurso;
-            fs.writeFile('maricula.txt',texto,(err) => {
-            if (err) throw (err);
-                console.log('se ha creado el archivo');
-                process.exit();
-            });
+            let texto = '<div style="border-style: solid;padding:10px;"> El estudiante '+nombreEstudiante+
+            ' con cedula '+ cedula + '<br>'+
+            'Se ha matriculado en ' + descripcionCurso+'</div>';
+
+            app.get('/', function (req, res) {
+                res.send(texto)
+            })
+
+            console.log('se ha desplegado el mensaje');
+
+           // fs.writeFile('matricula.txt',texto,(err) => {
+           // if (err) throw (err);
+           //     console.log('se ha creado el archivo');
+           //     process.exit();
+           // });
         }
         else{
             console.log('Ha ingresado un ID que no corresponde a ningun curso');
@@ -81,3 +102,5 @@ const argv = require('yargs')
             .argv
 
 matricularAlumno(argv.i,argv.n,argv.x);
+ 
+app.listen(3000)
